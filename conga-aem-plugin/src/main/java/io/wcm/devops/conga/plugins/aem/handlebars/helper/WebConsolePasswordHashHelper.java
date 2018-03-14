cb5db73af761565f21ef0f1bdece692f3f04caef
sseifert <sseifert@pro-vision.de>
2018-03-14 22:41:13 +0100

    add file header, set next version to 1.7.0

--- conga-aem-plugin/src/main/java/io/wcm/devops/conga/plugins/aem/handlebars/helper/WebConsolePasswordHashHelper.java
+++ conga-aem-plugin/src/main/java/io/wcm/devops/conga/plugins/aem/handlebars/helper/WebConsolePasswordHashHelper.java
@@ -1,15 +1,36 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2018 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package io.wcm.devops.conga.plugins.aem.handlebars.helper;
-
-import com.github.jknack.handlebars.Options;
-import io.wcm.devops.conga.generator.spi.handlebars.HelperPlugin;
-import io.wcm.devops.conga.generator.spi.handlebars.context.HelperContext;
-import org.apache.jackrabbit.oak.spi.security.user.util.PasswordUtil;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+
+import org.apache.jackrabbit.oak.spi.security.user.util.PasswordUtil;
+
+import com.github.jknack.handlebars.Options;
+
+import io.wcm.devops.conga.generator.spi.handlebars.HelperPlugin;
+import io.wcm.devops.conga.generator.spi.handlebars.context.HelperContext;
 
 /**
  * Handlebars helper that builds a password hash for a given password string for the Apache Felix Webconsole (felix.webconsole.password).
@@ -106,4 +127,5 @@ public class WebConsolePasswordHashHelper implements HelperPlugin<Object> {
 
     return String.format("{%s}%s", hashAlgorithm.toLowerCase(), hashedPasswordBase64);
   }
+
 }
--- conga-aem-plugin/src/test/java/io/wcm/devops/conga/plugins/aem/handlebars/helper/WebConsolePasswordHashHelperTest.java
+++ conga-aem-plugin/src/test/java/io/wcm/devops/conga/plugins/aem/handlebars/helper/WebConsolePasswordHashHelperTest.java
@@ -1,16 +1,36 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2018 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package io.wcm.devops.conga.plugins.aem.handlebars.helper;
-
-import io.wcm.devops.conga.generator.spi.handlebars.HelperPlugin;
-import io.wcm.devops.conga.generator.util.PluginManagerImpl;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.io.IOException;
 
 import static io.wcm.devops.conga.plugins.aem.handlebars.helper.TestUtils.executeHelper;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import io.wcm.devops.conga.generator.spi.handlebars.HelperPlugin;
+import io.wcm.devops.conga.generator.util.PluginManagerImpl;
 
 public class WebConsolePasswordHashHelperTest {
 
@@ -62,4 +82,5 @@ public class WebConsolePasswordHashHelperTest {
     Object passwordHash = executeHelper(helper, PASSWORD_PLAIN, new MockOptions().withHash(WebConsolePasswordHashHelper.HASH_OPTION_ENCODING, "invalid"));
     assertEquals(PASSWORD_HASH_SHA256, passwordHash);
   }
+
 }
