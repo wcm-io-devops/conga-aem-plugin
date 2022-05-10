fd1c0efd671cddf1e15ecbb055a4df0be12621aa
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-05-10 10:28:50 +0200

    Revert "ignore duplicate entries"
    This reverts commit 444dcd8a6931d9328c543e10195ba5cf098e5e52.

--- tooling/conga-aem-maven-plugin/src/main/java/io/wcm/devops/conga/plugins/aem/maven/allpackage/AllPackageBuilder.java
+++ tooling/conga-aem-maven-plugin/src/main/java/io/wcm/devops/conga/plugins/aem/maven/allpackage/AllPackageBuilder.java
@@ -297,7 +297,6 @@ public final class AllPackageBuilder {
 
     // build content package
     // if auto dependencies is active: build separate "dependency chains" between mutable and immutable packages
-    Set<String> packageFilePaths = new HashSet<>();
     try (ContentPackage contentPackage = builder.build(targetFile)) {
       for (ContentPackageFileSet fileSet : contentPackageFileSets) {
         for (String environmentRunMode : fileSet.getEnvironmentRunModes()) {
@@ -323,16 +322,9 @@ public final class AllPackageBuilder {
             try {
               for (TemporaryContentPackageFile processedFile : processedFiles) {
                 String path = buildPackagePath(processedFile, rootPath, environmentRunMode);
-                if (packageFilePaths.add(path)) {
-                  contentPackage.addFile(path, processedFile.getFile());
-                  if (log.isDebugEnabled()) {
-                    log.debug("  Add " + processedFile.getPackageInfoWithDependencies());
-                  }
-                }
-                else {
-                  if (log.isDebugEnabled()) {
-                    log.debug("  Ignore duplicate: " + processedFile.getPackageInfoWithDependencies());
-                  }
+                contentPackage.addFile(path, processedFile.getFile());
+                if (log.isDebugEnabled()) {
+                  log.debug("  Add " + processedFile.getPackageInfoWithDependencies());
                 }
               }
             }
