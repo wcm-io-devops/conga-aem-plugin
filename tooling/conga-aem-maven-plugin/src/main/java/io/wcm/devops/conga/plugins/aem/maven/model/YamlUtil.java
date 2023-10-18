43bee1645dd4c8727012913119f5d94ccd54f0bb
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-10-18 11:35:16 +0200

    remove unused import

--- tooling/conga-aem-maven-plugin/src/main/java/io/wcm/devops/conga/plugins/aem/maven/model/YamlUtil.java
+++ tooling/conga-aem-maven-plugin/src/main/java/io/wcm/devops/conga/plugins/aem/maven/model/YamlUtil.java
@@ -21,7 +21,6 @@ package io.wcm.devops.conga.plugins.aem.maven.model;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
